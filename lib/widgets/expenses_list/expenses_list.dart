eb6893c59e6f0f04e09fbec78a9216a1c82b1331
nilaybesli <nil.besli@hotmail.com>
2024-03-08 01:06:21 +0300

    added dark theme

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -12,13 +12,17 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (ctx, index) =>
-          Dismissible(
-              key: ValueKey(expenses[index]),
-              onDismissed: (direction){
-                onRemoveExpense(expenses[index]);
-              } ,
-              child: ExpenseItem(expenses[index])),
+      itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(expenses[index]),
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+            margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+          ),
+          onDismissed: (direction) {
+            onRemoveExpense(expenses[index]);
+          },
+          child: ExpenseItem(expenses[index])),
       itemCount: expenses.length,
     );
   }
